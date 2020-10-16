3cbb5a831abd7cea356a7105444d452af099aae0
alseny <alsenydiallo0409@gmail.com>
2020-10-16 15:22:45 +0000

    ajout fonction connexion

--- api/database.php
+++ api/database.php
@@ -2,4 +2,17 @@
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
     header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");    
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', 'Passer');
+    define('DB_NAME', 'SenActe');
+
+    function connexion(){
+        try {
+            $bdd = new PDO('mysql:host=DB_HOST;dbname=DB_NAME','DB_USER','DB_PASS',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        } catch (Exception $e) {
+            die('Probleme de connexion: '.$e->getMessage());
+        }
+        
+    }
 ?>
